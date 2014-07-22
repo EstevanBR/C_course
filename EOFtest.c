e2550189de9336c977b5513de716eb3e657ce362
Estevan Hernandez <estevan@bluerocket.us>
2014-07-22 14:54:10 -0700

    EOF? end of fricks

--- EOFtest.c
+++ EOFtest.c
@@ -3,10 +3,17 @@
 int main(void)
 {
 	int ch;
-	while ((ch = getchar()))
-	{	
-		
+	
+	while ((ch = getchar()) != EOF)
+	{
 		putchar(ch);
 	}
+	/*
+	do{
+		ch = getchar();
+		putchar(ch);
+	}while(ch != EOF);
+	*/
+
 	return 0;
 }
