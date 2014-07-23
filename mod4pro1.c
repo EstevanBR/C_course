e224c435ca7232f85509ebd0dc2d9de3ca047f14
Estevan Hernandez <estevan@bluerocket.us>
2014-07-22 17:19:44 -0700

    more work

--- mod4pro1.c
+++ mod4pro1.c
@@ -1,1 +1,30 @@
-mod4pro1.c
+/************************************************
+    * filename: mod4pro1.c
+    * Assignment: Module 4, Programming Assignnment 1
+    * Name: Estevan Hernandez
+    * Date Created: July 22, 2014
+    *
+    * Write a program that prompts the user to enter
+    * in characters, an equal sign, and an integer.
+    * The program then reads and discards all characters
+    * from standard input using getchar() until it
+    * encounters the "=".  The program then reads the
+    * integer value that follows the "=" using scanf()
+    * and outputs the value back to the user.
+    * For example:
+	*
+    * Enter some text, equal sign and an integer: The number = 16439
+	* The integer you entered is:   16439
+    *************************************************/
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	printf("Please enter some text, followed by an equals sign and lastly and integer:\n");
+	while((getchar() != '='))
+		continue;
+	scanf("%d", &num);
+	printf("the integer you entered is: %d\n", num);
+
+	return 0;
+}
---
+++ mod4pro2.c
@@ -0,0 +1,19 @@
+/************************************************
+    * filename: mod4pro2.c
+    * Assignment: Module 4, Programming Assignnment 2
+    * Name: Estevan Hernandez
+    * Date Created: July 22, 2014
+    *
+    * Write a program that prompts the user to enter
+    two values of data type "double".  Implement a
+    function called print_doubles() that takes the
+    two arguments of data type "double" and uses
+    printf() to display both.
+    *************************************************/
+#include <stdio.h>
+double print_doubles(double num, double num2);
+int main(void)
+{
+	
+}
+
