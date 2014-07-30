59771fa94ee67a6549573f3a60c327ec3097f28a
Estevan Hernandez <estevan@bluerocket.us>
2014-07-30 10:34:44 -0700

    more source code files

--- array.c
+++ array.c
@@ -4,8 +4,15 @@ int main(void)
 	int i;
 	int test[5] = {1,2,3,4,5};
 	//printf("plese enter number 0-5\n");
-	//scanf("%d", &i);
-	printf("test =\t\t%p\n"
-			"&test[0] =\t%p\n", test, &test[0]);
+	//scanf("%20d", &i);
+	printf("test     =%20p\n"
+		   "&test[0] =%20p\n", test, &test[0]);
+	printf("*test+1  =%20d\n"
+		   "test[1]  =%20d\n", *test+1, test[1]);
+	printf("*test+1  =%20d\n"
+		   "test[1]  =%20d\n", *test+2, test[2]);
+	printf("*test+1  =%20d\n"
+		   "test[1]  =%20d\n", *test+3, test[3]);
+
 	return 0;
 }
